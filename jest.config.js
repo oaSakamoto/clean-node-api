0e092ee5a4249f0e2a47cee194258dfb5501e0d1
Olimpio Sakamoto <oasakamoto@proton.me>
2024-03-12 02:54:22 -0300

    chore: adiciona jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: { '.+\\.ts$': 'ts-jest' }
+}
+
+module.exports = config
