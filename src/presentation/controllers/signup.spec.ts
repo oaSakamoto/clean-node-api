bff31c9baac88bb57a1c0ace2e49b509a2a87d95
Olimpio Sakamoto <oasakamoto@proton.me>
2024-03-13 01:57:29 -0300

    test: garante que SignUpController lida com EmailValidator
    SignUpCOntroller chama EmailValidator com o email correto

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -96,4 +96,18 @@ describe('SignUp Controller', () => {
       new InvalidParamError('email')
     )
   })
+  test('Deve chamar EmailValidator com o email correto', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
