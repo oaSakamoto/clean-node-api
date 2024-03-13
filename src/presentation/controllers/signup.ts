3b0d7994c410db738d1c0b02188cbeb9a998d7e0
Olimpio Sakamoto <oasakamoto@proton.me>
2024-03-13 03:12:55 -0300

    feat: garante que SignUpController lida com confimação de password
    SignUpController retorna 400 se a confimação do password falhar

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -16,6 +16,9 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
