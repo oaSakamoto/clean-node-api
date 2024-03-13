38d35eab885286d01cfc715068921661f6e1bd86
Olimpio Sakamoto <oasakamoto@proton.me>
2024-03-12 23:47:10 -0300

    test: garante que SignupContoller retorna 400 se nome não for fornecido

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Deve retornar 400 se o nome não for fornecido', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
