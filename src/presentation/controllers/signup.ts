fb8f70724d22786c5b21cf5dd08d4379eb2988c2
Olimpio Sakamoto <oasakamoto@proton.me>
2024-03-13 03:16:26 -0300

    refactor: remove codigo duplicado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -16,10 +16,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      const emailIsValid = this.emailValidator.isValid(email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
       }
